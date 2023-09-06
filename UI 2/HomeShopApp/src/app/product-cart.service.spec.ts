04890e332ed54a40ce3579f675b4d500d78606df
Rishi <rishighsh@gmail.com>
2023-09-06 11:29:46 +0530

    test  19

--- UI 2/HomeShopApp/src/app/product-cart.service.spec.ts
+++ UI 2/HomeShopApp/src/app/product-cart.service.spec.ts
@@ -1,16 +1,16 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
-import { productCartOrderService } from './product-cart.service';
+import { ProductCartService } from './product-cart.service';
 
 describe('Service: ProductCart', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [productCartOrderService]
+      providers: [ProductCartService]
     });
   });
 
-  it('should ...', inject([productCartOrderService], (service: productCartOrderService) => {
+  it('should ...', inject([ProductCartService], (service: ProductCartService) => {
     expect(service).toBeTruthy();
   }));
 });
