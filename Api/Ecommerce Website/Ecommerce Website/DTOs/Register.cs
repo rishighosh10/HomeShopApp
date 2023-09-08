6e0d79609d7c98ff4b5ef6962eb30abad613e965
Rishi GHosh <67872576+rishighosh10@users.noreply.github.com>
2023-09-08 19:02:08 +0530

    Add files via upload

---
+++ Api/Ecommerce Website/Ecommerce Website/DTOs/Register.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce_Website.DTOs
+{
+    public class Register
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Contact { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public int PinCode { get; set; }
+
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+
+        private string role;
+        public string Role
+        {
+            get { return role; }
+            set { role = "Customer"; }
+        }
+    }
+}
